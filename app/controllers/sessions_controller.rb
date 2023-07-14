d1571187acaf56a2c1c10a4c2d51d6b820702f29
Jason Park <jjpark987@gmail.com>
2023-07-14 10:22:59 -0700

    commit tree 53d5ce24d3a9fa87cf841ef64a25da95e6ed5bfc

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    # post '/login'
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
+    # delete '/logout'
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
